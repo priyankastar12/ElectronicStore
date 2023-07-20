a76ffd982cd554d54e1f416d7b3ebe86c76b3300
pravin11793 <pravinpawar11793@gmail.com>
2023-07-20 17:46:46 +0530

    project updated to the github ImageResponse is done

---
+++ src/main/java/com/bikkadit/electronic/store/dto/ImageResponse.java
@@ -0,0 +1,21 @@
+package com.bikkadit.electronic.store.dto;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageResponse {
+
+          private String imageName;
+          private String message;
+          private boolean success;
+
+          private HttpStatus status;
+
+
+    }
+
