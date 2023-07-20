654906f4e3503fa25381b9bd4633057f0ffdf450
pravin11793 <pravinpawar11793@gmail.com>
2023-07-20 17:43:48 +0530

    project updated to the github AppConstant is done

---
+++ src/main/java/com/bikkadit/electronic/store/helper/AppConstant.java
@@ -0,0 +1,17 @@
+package com.bikkadit.electronic.store.helper;
+
+public class AppConstant {
+
+        public static final String USER_NOT_FOUND="User not found !!";
+        public static final String USER_DELETED="User is deleted successfully !!";
+
+        public static final String CATEGORY_NOT_FOUND="Category not found with given Id !!";
+
+        public static final String CATEGORY_DELETED="Category is deleted successfully !!";
+
+        public static final String PRODUCT_NOT_FOUND="Product not found with given Id !!";
+
+        public static final String PRODUCT_DELETED="Product is deleted successfully !!";
+
+
+}
