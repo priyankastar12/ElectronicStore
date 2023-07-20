1452c697619deb731ea3016fe48b47b90917515c
pravin11793 <pravinpawar11793@gmail.com>
2023-07-20 16:43:14 +0530

    project updated to the github in user module UserDto is completed

--- src/main/java/com/bikkadit/electronic/store/dto/UserDto.java
+++ src/main/java/com/bikkadit/electronic/store/dto/UserDto.java
@@ -1,6 +1,12 @@
 package com.bikkadit.electronic.store.dto;
 
+import com.bikkadit.electronic.store.validate.ImageNameValid;
 import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -11,15 +17,25 @@ public class UserDto {
 
     private String userId;
 
+
+    @Size(min = 3,max = 20,message="Invalid Name !!")
     private String name;
 
+
+    @NotBlank(message = "Email is required !!")
+    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)+[a-z]{2,5}$",message = "Invalid User Email !!")
     private String email;
+
+    @NotBlank(message = "Password is required !!")
 
     private String password;
 
+    @Size(min = 3,max = 6, message = "Invalid gender !!")
     private String gender;
 
+    @NotBlank(message = "Write something about yourself !!")
     private String about;
 
+    @ImageNameValid
     private String imageName;
 }
