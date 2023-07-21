02d8bd8f5d1672a7b4383fb6c862ebe4efc67edc
pravin11793 <pravinpawar11793@gmail.com>
2023-07-21 15:21:44 +0530

    project updated to the github ProductDto class is done

---
+++ src/main/java/com/bikkadit/electronic/store/dto/ProductDto.java
@@ -0,0 +1,35 @@
+package com.bikkadit.electronic.store.dto;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductDto {
+
+    private String productId;
+
+    private String title;
+
+
+    private String description;
+
+    private int price;
+
+    private int discountedPrice;
+
+    private int quantity;
+
+    private Date addedDate;
+
+    private boolean live ;
+
+    private boolean stock;
+
+    private  String productImage;
+}
